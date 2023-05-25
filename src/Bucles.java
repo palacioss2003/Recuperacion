1afb0333e2d609d9e496eac1320e3f41a40f5c4e
palacioss2003 <palacioslagunamanuel@gmail.com>
2023-05-25 13:25:07 +0200

    El commit del .java de Bucles

---
+++ src/Bucles.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Bucles {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduce un numero:");
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Introduce un numero:");
+		int num2 = sc.nextInt();
+
+		for (int i = 1; i <= num2; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+
+			}
+
+		}
+
+	}
+
+}
